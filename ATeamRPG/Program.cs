028d551163ed49933ac3a8fa9e9dab0284cd7de3
Rasmus Olofsson <rasmus.olofsson1992@gmail.com>
2016-02-16 11:19:00 +0100

    hello

--- ATeamRPG/Program.cs
+++ ATeamRPG/Program.cs
@@ -8,15 +8,28 @@ namespace ATeamRPG
 {
     class Program
     {
+        static Player player1;
+        static Player player2;
         //Working????
         static void Main(string[] args)
         {
-            Console.WriteLine("A Team");
+            Start();
         }
 
-        static void Poo()
+        static void Start()
         {
-            Console.WriteLine("Oj, här luktade det illa!");
+            string name;
+            Console.WriteLine("Welcome to the game");
+            Console.WriteLine("Insert desired name: ");
+            Console.WriteLine("Player 1 : ");
+
+            name = Console.ReadLine();
+            player1 = new Player(name);
+            Console.WriteLine("Player 2 : ");
+            name = Console.ReadLine();
+            player2 = new Player(name);
+
+            Console.WriteLine(player1.Name + player2.Name);
         }
         static void JoelLovesToBurn()
         {
