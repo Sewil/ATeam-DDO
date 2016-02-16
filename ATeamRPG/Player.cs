a70260f0bf8778cb2239a3a2e570e58c050acaaa
Rasmus Olofsson <rasmus.olofsson1992@gmail.com>
2016-02-16 10:48:37 +0100

    Player class

---
+++ ATeamRPG/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATeamRPG
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public int XPosition { get; set; }
+        public int YPosition { get; set; }
+        public int Health { get; set; }
+        public int Gold { get; set; }
+        public int Damage { get; set; }
+    }
+}
