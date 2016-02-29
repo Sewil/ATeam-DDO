50aecea6549740dc13f4f703c7b11831a2b7cbc5
Simon Haga <simon.haga94@gmail.com>
2016-02-29 20:08:17 +0100

    fanns tydligen inte

---
+++ DDOServer/DDOServer.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DDOServer {
+    class DDOServer {
+        static Socket socket = null;
+        const Int32 LISTENERBACKLOG = 100;
+        const Int32 BUFFERLENGTHMAP = 2000;
+        const int BUFFERLENGTHPLAYER = 100;
+        const String IPADDRESS = "127.0.0.1";
+        const Int32 PORT = 8001;
+        static IPAddress ipAddress = IPAddress.Parse(IPADDRESS);
+        static IPEndPoint localEndPoint = new IPEndPoint(ipAddress, PORT);
+        static UTF8Encoding encoding = new UTF8Encoding();
+        static IPEndPoint remoteEndPoint = new IPEndPoint(ipAddress, 8000);
+
+        static void ReceiveStartRequest(Socket socket, out string name) {
+            name = null;
+
+            byte[] bufferIn = new Byte[BUFFERLENGTHMAP];
+            socket.Receive(bufferIn);
+
+            string request = encoding.GetString(bufferIn).TrimEnd('\0').Trim();
+            Console.WriteLine("Received request from " + socket.RemoteEndPoint + ": " + request);
+
+            name = request.Split(' ')[2];
+        }
+
+        static char RecieveDirection(Socket socket) {
+            byte[] bufferIn = new Byte[BUFFERLENGTHMAP];
+            socket.Receive(bufferIn);
+
+            string request = encoding.GetString(bufferIn).TrimEnd('\0').Trim();
+            Console.WriteLine("Received request from " + socket.RemoteEndPoint + ": " + request);
+
+            return Convert.ToChar(request);
+        }
+
+        static void SendMap(Socket[] sockets, string mapStr) {
+            byte[] bufferOut = encoding.GetBytes(mapStr);
+            foreach (var socket in sockets) {
+                socket.Send(bufferOut);
+            }
+        }
+
+        static void SendPlayerInfo(Socket[] sockets, params Player[] players) {
+            string response = null;
+            byte[] bufferOut = new Byte[BUFFERLENGTHPLAYER];
+
+            for (int i = 0; i < players.Length; i++) {
+                response = ($"Name: {players[i].Name} Health: {players[i].Health} Damage: {players[i].Damage} Gold: {players[i].Gold}");
+                bufferOut = encoding.GetBytes(response);
+                sockets[i].Send(bufferOut);
+            }
+        }
+
+        static void PlayDDO(object parameter) {
+            Socket[] sockets = (Socket[])parameter;
+            string playerOneName = null;
+            string playerTwoName = null;
+            Map map = null;
+            string mapStr = null;
+            char direction;
+
+
+            try {
+                ReceiveStartRequest(sockets[0], out playerOneName);
+                ReceiveStartRequest(sockets[1], out playerTwoName);
+
+                var players = new Player[sockets.Length];
+                players[0] = new Player(playerOneName);
+                players[1] = new Player(playerTwoName);
+
+                map = Map.Load(players);
+                mapStr = map.MapToString();
+
+                SendMap(sockets, mapStr);
+                SendPlayerInfo(sockets, players);
+
+                while (true) {
+                    direction = RecieveDirection(sockets[0]);
+                    map.MovePlayer(direction, players[0].Name);
+                    mapStr = map.MapToString();
+                    SendMap(sockets, mapStr);
+                    SendPlayerInfo(sockets, players);
+
+                    direction = RecieveDirection(sockets[1]);
+                    map.MovePlayer(direction, players[1].Name);
+                    mapStr = map.MapToString();
+                    SendMap(sockets, mapStr);
+                    SendPlayerInfo(sockets, players);
+                }
+            } catch (Exception exception) {
+                Console.WriteLine("Exception: " + exception.Message);
+                Console.WriteLine("Stack trace: " + exception.StackTrace);
+                Console.ReadLine();
+            } finally {
+                if (sockets[0] != null)
+                    sockets[0].Close();
+                if (sockets[1] != null)
+                    sockets[1].Close();
+            }
+        }
+
+        static void ConnectToMasterServer() {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(remoteEndPoint);
+            Console.WriteLine("Connected to MasterServer.");
+        }
+
+        static void Main(string[] args) {
+            Socket listeningSocket = null;
+            Socket[] sockets = new Socket[2];
+            ConnectToMasterServer();
+
+            try {
+                listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                listeningSocket.Bind(localEndPoint);
+                while (true) {
+                    listeningSocket.Listen(LISTENERBACKLOG);
+                    sockets[0] = listeningSocket.Accept();
+                    Console.WriteLine("Player 1 connected");
+
+                    string id1 = "1";
+                    byte[] bufferOut1 = encoding.GetBytes(id1);
+                    sockets[0].Send(bufferOut1);
+
+                    listeningSocket.Listen(LISTENERBACKLOG);
+                    sockets[1] = listeningSocket.Accept();
+                    Console.WriteLine("Player 2 connected");
+
+                    string id2 = "2";
+                    byte[] bufferOut2 = encoding.GetBytes(id2);
+                    sockets[1].Send(bufferOut2);
+
+                    ParameterizedThreadStart threadStart = new ParameterizedThreadStart(PlayDDO);
+                    Thread thread = new Thread(threadStart);
+                    thread.Start(sockets);
+                }
+            } catch (Exception exception) {
+                Console.WriteLine(exception.Message);
+                Console.ReadLine();
+            } finally {
+                if (listeningSocket != null)
+                    listeningSocket.Close();
+            }
+        }
+    }
+}
---
+++ DDOServer/Map.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DDOServer {
+    class Map {
+        long spawnTimeMonster;
+        long spawnTimeHealthPotion;
+        const int GOLD_ROUND_TURNS = 50;
+        const int MONSTER_SPAWN_TURNS = 5;
+        public const int MAXSPAWNEDMONSTERS = 10;
+        public const int MAXSPAWNEDHEALTHPOTIONS = 10;
+        int turn;
+        public int monsterCount;
+        public int SpawnedMonsters { get; set; }
+        public int SpawnedHealthPotions { get; set; }
+        public int Turn {
+            get {
+                return turn;
+            }
+            set {
+                turn = value;
+                if (turn % GOLD_ROUND_TURNS == 0) {
+                    OnGoldRound(this);
+                }
+                if (turn % MONSTER_SPAWN_TURNS == 0 && monsterCount >= 10) {
+                    OnMonsterSpawn(this);
+                    monsterCount++;
+                }
+            }
+        }
+        public Player[] players;
+        public const int WIDTH = 75;
+        public const int HEIGHT = 20;
+        public Cell[,] Cells = new Cell[HEIGHT, WIDTH];
+        const double FOREST_CHANCE = 0.5;
+
+        private Map() {
+            var random = new Random();
+            for (int y = 0; y < HEIGHT; y++) {
+                for (int x = 0; x < WIDTH; x++) {
+                    if (random.NextDouble() <= FOREST_CHANCE) {
+                        Cells[y, x] = new Cell(y, x, CellType.Forest);
+                    } else {
+                        Cells[y, x] = new Cell(y, x, CellType.Ground);
+                    }
+                }
+            }
+            for (int i = 0; i < random.Next(1, 4); i++) {
+                Cells = DoSimulationStep();
+            }
+        }
+        public event Action<Map> GoldRound;
+        public event Action<Map> MonsterSpawn;
+        public void OnGoldRound(Map m) {
+            GoldRound?.Invoke(m);
+        }
+        public void OnMonsterSpawn(Map m) {
+            MonsterSpawn?.Invoke(m);
+        }
+
+        public static Map Load(params Player[] players) {
+            var map = new Map {
+                players = players
+            };
+            var monsters = new Monster();
+            map.MonsterSpawn += (m) => {
+                m.SpawnMonsters();
+            };
+            map.GoldRound += (m) => {
+                m.PlaceGold();
+            };
+            foreach (var player in map.players) {
+                player.Died += (p) => {
+                    map.SpawnPlayers(p as Player); // respawn on death
+                    (p as Player).Health = Player.HEALTH;
+                };
+            }
+            map.PlaceGold();
+            map.SpawnPlayers(players);
+            return map;
+        }
+
+        int CountForestNeighbors(int y, int x) {
+            int neighbors = 0;
+            for (int i = -1; i <= 1; i++) {
+                for (int j = -1; j <= 1; j++) {
+                    int neighborY = i + y;
+                    int neighborX = j + x;
+                    bool selfCell = i == 0 && j == 0;
+                    bool outsideCell = neighborY < 0 || neighborX < 0 || neighborY >= HEIGHT || neighborX >= WIDTH;
+
+                    if (!selfCell && (outsideCell || Cells[neighborY, neighborX].CellType == CellType.Forest)) {
+                        neighbors++;
+                    }
+                }
+            }
+
+            return neighbors;
+        }
+
+        Cell[,] DoSimulationStep() {
+            var newCells = new Cell[HEIGHT, WIDTH];
+            for (int y = 0; y < HEIGHT; y++) {
+                for (int x = 0; x < WIDTH; x++) {
+                    int neighbours = CountForestNeighbors(y, x);
+                    bool borderCell = y == 0 || y == HEIGHT - 1 || x == 0 || x == WIDTH - 1;
+
+                    if (borderCell || neighbours > 4) {
+                        newCells[y, x] = new Cell(y, x, CellType.Forest);
+                    } else if (neighbours <= 4) {
+                        newCells[y, x] = new Cell(y, x, CellType.Ground);
+                    }
+                }
+            }
+            return newCells;
+        }
+
+        public string MapToString() {
+            string mapStr = null;
+            for (int y = 0; y < HEIGHT; y++) {
+                for (int x = 0; x < WIDTH; x++) {
+                    var cell = Cells[y, x];
+                    if (cell.HasGold) {
+                        mapStr += "$";
+                    } else if (cell.HasPlayer) {
+                        mapStr += "@";
+                    } else if (cell.HasHealthPotion) {
+                        mapStr += "P";
+
+                    } else if (cell.HasMonster) {
+                        mapStr += "M";
+                    } else if (cell.CellType == CellType.Forest) {
+                        mapStr += "#";
+                    } else if (cell.CellType == CellType.Ground) {
+                        mapStr += ".";
+                    }
+
+                }
+            }
+
+            return mapStr;
+        }
+
+        double goldChance = 0.1;
+        int[] goldRange = { 100, 1000 };
+
+        public void PlaceGold() {
+            var random = new Random();
+            foreach (var cell in Cells) {
+                if (cell.IsGoldable && random.NextDouble() <= goldChance) {
+                    cell.Gold += random.Next(goldRange[0], goldRange[1] + 1);
+                }
+            }
+        }
+
+        public void SpawnPlayers(params Player[] players) {
+            var random = new Random();
+            foreach (var player in players) {
+                List<Cell> emptyCells = new List<Cell>();
+                foreach (var cell in Cells) {
+                    if (cell.IsSpawnable) {
+                        emptyCells.Add(cell);
+                    }
+                }
+                if (emptyCells.Count > 0) {
+                    var randomCell = emptyCells[random.Next(0, emptyCells.Count)];
+                    randomCell.Player = player;
+                } else {
+                    throw new Exception("Error spawning player. No free cells.");
+                }
+            }
+        }
+
+        public void SpawnHealthPotion() {
+            // Place in infinite loop if we want to thread it in background
+            if (DateTime.Now.Ticks > spawnTimeHealthPotion) {
+                var random = new Random();
+
+                var availableCells = new List<Cell>();
+                foreach (var cell in Cells) {
+                    if (cell.IsSpawnable && SpawnedHealthPotions < MAXSPAWNEDHEALTHPOTIONS)
+                        availableCells.Add(cell);
+                }
+                var randomCell = availableCells[random.Next(0, availableCells.Count)];
+                randomCell.HealthPotion = new HealthPotion();
+                SpawnedHealthPotions += 1;
+                spawnTimeHealthPotion = DateTime.Now.Ticks + 50000000;
+            }
+        }
+
+        public void SpawnMonster() {
+            // Place in infinite loop if we want to thread it in background
+            if (DateTime.Now.Ticks > spawnTimeMonster) {
+                var rnd = new Random();
+
+                var emptyCells = new List<Cell>();
+                foreach (var cell in Cells) {
+                    if (cell.IsSpawnable && SpawnedMonsters < MAXSPAWNEDMONSTERS)
+                        emptyCells.Add(cell);
+                }
+                var rndCell = emptyCells[rnd.Next(0, emptyCells.Count)];
+                rndCell.Monster = new Monster();
+                SpawnedMonsters += 1;
+                spawnTimeMonster = DateTime.Now.Ticks + 50000000;
+            }
+        }
+
+        public void SpawnMonsters(params Monster[] monsters) {
+            var random = new Random();
+            foreach (var monster in monsters) {
+
+            }
+        }
+        Cell FindPlayer(string playerName) {
+            foreach (var cell in Cells) {
+                if (cell.HasPlayer) {
+                    if (cell.Player.Name == playerName) {
+                        return cell;
+                    }
+                }
+            }
+
+            return null;
+        }
+        public void MovePlayer(char direction, string playerName) {
+            var currentCell = FindPlayer(playerName);
+            Cell newCell = null;
+            switch (direction) {
+                case '↑':
+                    if (currentCell.Y > 0) {
+                        newCell = Cells[currentCell.Y - 1, currentCell.X];
+                    }
+                    break;
+                case '→':
+                    if (currentCell.X < WIDTH - 1) {
+                        newCell = Cells[currentCell.Y, currentCell.X + 1];
+                    }
+                    break;
+
+                case '↓':
+                    if (currentCell.Y < HEIGHT - 1) {
+                        newCell = Cells[currentCell.Y + 1, currentCell.X];
+                    }
+                    break;
+
+                case '←':
+                    if (currentCell.X > 0) {
+                        newCell = Cells[currentCell.Y, currentCell.X - 1];
+                    }
+                    break;
+            }
+
+            if (newCell != null) {
+                Turn++;
+                if (newCell.HasPlayer) {
+                    newCell.Player.Health -= currentCell.Player.Damage;
+                } else if (newCell.HasMonster) {
+                    newCell.Monster.Health -= currentCell.Player.Damage;
+                } else if (newCell.IsWalkable) {
+                    var p = currentCell.Player;
+                    currentCell.OnCharacterLeft();
+                    newCell.OnCharacterArrived(p);
+                }
+            }
+        }
+    }
+}
