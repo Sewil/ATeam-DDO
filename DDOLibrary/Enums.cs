ae3bbb7f386d60b54f10a0a70e19b0050b7fb63d
Simon Haga <simon.haga94@gmail.com>
2016-03-16 16:22:28 +0100

    update

---
+++ DDOLibrary/Enums.cs
@@ -0,0 +1,38 @@
+﻿namespace DDOLibrary {
+    public enum Action {
+        Add, Remove, Update
+    }
+    public enum CellType {
+        Forest, Ground
+    }
+    public enum Direction {
+        Up, Right, Down, Left
+    }
+    public enum TransferMethod {
+        REQUEST,
+        RESPONSE
+    }
+    public enum DataType {
+        NONE,
+        TEXT,
+        JSON
+    }
+    public enum ResponseStatus {
+        NONE,
+        OK,
+        LIMIT_REACHED,
+        UNAUTHORIZED,
+        OUT_OF_BOUNDS,
+        NOT_FOUND,
+        NOT_READY,
+        BAD_REQUEST
+    }
+    public enum RequestStatus {
+        NONE,
+        UPDATE_STATE,
+        START,
+        LOGIN,
+        MOVE,
+        SEND_CHAT_MESSAGE
+    }
+}
